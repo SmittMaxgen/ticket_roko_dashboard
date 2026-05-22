ae0cfe35cf724181128f23570bab0be2a828c15c
Your Name <your.email@example.com>
2026-05-22 16:13:29 +0530

    Chaneg!

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,7 +4,7 @@ import axios from "axios";
 export const API_BASE_URL = "api.ticketroko.retailian.in";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000/api", // ✅ remove the full domain
+  baseURL: "/api", // ✅ remove the full domain
 });
 
 // attach token
